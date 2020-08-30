4c075de508744ec0290ad09bd1638c4a888235ae
Colin <colin.ingram@bimobject.com>
2020-08-30 15:13:32 +0200

    Update proto and v1 dataschema (WIP)

---
+++ student/src/data/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE dbo.Student (
+  Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+  Name NVARCHAR(150),
+  IdentifiesAs NVARCHAR(20),
+  DOB Date,
+  SocialSecurityNumber NVARCHAR(50),
+);
+
+CREATE TABLE dbo.StudentParent (
+  StudentId UNIQUEIDENTIFIER,
+  ParentId UNIQUEIDENTIFIER,
+);
+
+CREATE TABLE dbo.Parent (
+  Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+  FirstName NVARCHAR(150),
+  LastName NVARCHAR(150),
+  SocialSecurityNumber NVARCHAR(50),
+  IdentifiesAs NVARCHAR(20),
+  AddressLine1  NVARCHAR(10) NOT NULL,
+  AddressLine2  NVARCHAR(10),
+  City  NVARCHAR(10) NOT NULL,
+  PostNumber NVARCHAR(10) NOT NULL,
+  SpouseId UNIQUEIDENTIFIER,
+);
+
+CREATE TABLE dbo.StudentChangeLog (
+  StudentId UNIQUEIDENTIFIER PRIMARY KEY,
+  EventDateTime DATETIME2,
+  EventType NVARCHAR(15),
+  PropertySnapshot NVARCHAR
+);
+
+CREATE TABLE dbo.ParentChangeLog (
+  ParentId UNIQUEIDENTIFIER PRIMARY KEY,
+  EventDateTime DATETIME2,
+  EventType NVARCHAR(15),
+  PropertySnapshot NVARCHAR
+);
