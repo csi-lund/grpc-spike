91acd25be6a35c4bf9d7e43fafd77c08998dd1ed
Colin <colin.ingram@bimobject.com>
2020-09-07 00:30:04 +0200

    create client and add new proto for queue

---
+++ waitlist/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WaitList
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new WaitList.Student(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
