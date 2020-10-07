6a1ec5b8f78f5dc7ab038754bc2a2f1ce3def840
Colin <colin.ingram@bimobject.com>
2020-10-07 22:51:16 +0200

    Add autgen packages and impl for autogen class based on protobuf file

---
+++ queue/QueueImpl.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Queue;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    public class QueueImpl : queueManager.queueManagerBase
+    {
+        public override Task<StudentPositionResponse> AddStudentToQueue(StudentQueueRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new StudentPositionResponse());
+        }
+
+        public override Task GetQueue(QueueAllRequest request, IServerStreamWriter<StudentPositionResponse> responseStream, ServerCallContext context)
+        {
+            return Task.FromResult(new StudentPositionResponse());
+        }
+
+        public override Task<StudentPositionResponse> GetStudentPosition(StudentQueueRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new StudentPositionResponse());
+        }
+    }
+}
--- queue/Startup.cs
+++ queue/Startup.cs
@@ -31,7 +31,6 @@ namespace queue
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
